b44654f3242eee6141c858144fbb56cf8f144657
Soohyun <nata1225@khu.ac.kr>
2021-10-08 10:47:39 +0900

    gugudan

---
+++ gugudan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+	cout << "구구단 출력" << endl;
+	for (int i =1; i < 9; i++) {
+		cout << i << "단" << endl;
+		for (int j = 1; j < 10; j++) {
+			cout << i << " * " << j << " = " << i * j << endl;;
+		}
+		cout << endl;
+	}
+
+
+}
