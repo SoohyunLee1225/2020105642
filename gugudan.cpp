3d3b17408fe3c054aa061c579b1d794aae205ff3
Yunhee Lim <lyhhh0320@gmail.com>
2021-10-08 11:01:18 +0900

    9단 추가

--- gugudan.cpp
+++ gugudan.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 
 	cout << "구구단 출력" << endl;
-	for (int i =1; i < 9; i++) {
+	for (int i =1; i <= 9; i++) {
 		cout << i << "단" << endl;
 		for (int j = 1; j < 10; j++) {
 			cout << i << " * " << j << " = " << i * j << endl;;
